6b131faad131d574d366a390047dd749c70fa88e
diana603 <stingray2091@gmail.com>
2020-08-14 22:47:26 -0400

    Updates

---
+++ precache-manifest.30f38746d1c685e895df5699442583c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71470126378708985dfd09ab31c71783",
+    "url": "/portfoliotwo/index.html"
+  },
+  {
+    "revision": "acec7bd66bac122677ef",
+    "url": "/portfoliotwo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00a9f6551b36495e40a3",
+    "url": "/portfoliotwo/static/js/2.366fadd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfoliotwo/static/js/2.366fadd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acec7bd66bac122677ef",
+    "url": "/portfoliotwo/static/js/main.8132cbfc.chunk.js"
+  },
+  {
+    "revision": "17dbab6b62982b8724ed",
+    "url": "/portfoliotwo/static/js/runtime-main.cb6ad302.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfoliotwo/static/media/logo.5d5d9eef.svg"
+  }
+]);
